f476468dc38679ba8867f939504d84d7b95fa80f
Fabian Falck <fabian.falck@web.de>
2018-07-20 13:47:46 +0100

    ROC implementation fixed and now fully functional

--- curves/ROC.py
+++ curves/ROC.py
@@ -17,53 +17,70 @@ import numpy as np
 
 
 
-def plot_roc(k_th_fold, ts_id, time, y_true, y_pred, model_labels, evaluation_path, default=False):
+def plot_roc(k_th_fold, ts_id, time, y_true, y_score, model_labels, evaluation_path, n_test_rounds=3, default=False):
     """
-    Plotting an ROC in 4 different versions (see the j loop below):
-        # j=1: x-y : FPR-TPR : unit-unit
-        # j=2: x-y : FPR-TPR : log-unit
-        # j=3: x-y : FNR-TNR : unit-unit
-        # j=4: x-y : FNR-TNR : log-unit
+    Plotting an ROC for time series data.
+
+    The ROC is plotted in such a way that the full data over all time series is split into 3 different sets ("round").
+    For each set, an ROC curve is computed. The resulting ROC and its confidence bounds are computed by computing
+    the average and standard deviation over the sub-ROCs of each set. Furthermore, the implementation is based on
+    and is made consistent with the sklearn curve computation.
+
+    The ROC is automatically plotted in 4 different versions (see the j loop below):
+    - j=1: x-y : FPR-TPR : unit-unit
+    - j=2: x-y : FPR-TPR : log-unit
+    - j=3: x-y : FNR-TNR : unit-unit
+    - j=4: x-y : FNR-TNR : log-unit
+
     Furthermore, the ROC can be used to produce the ROCs for one model
     (by passing the parameters as numpy.arrays as directly extracted
     from extract_columns(...)) or for comparing multiple models (by
-    passing a list of numpy.arrays to each of the parameters). Both versions
-    also have different styles to display them.
+    passing a list of numpy.arrays to each of the parameters).
+    Both versions also have different styles to display them: While the "single" ROC displays the sub-set ROCs ("round") as lines,
+    the "multi" ROC only plots the average and confidence bound for each model.
 
     :param k_th_fold: numpy.array (if one model) or list (if multiple models)
     :param ts_id: numpy.array (if one model) or list (if multiple models)
     :param time: numpy.array (if one model) or list (if multiple models)
     :param y_true: numpy.array (if one model) or list (if multiple models)
-    :param y_pred: numpy.array (if one model) or list (if multiple models)
+    :param y_score: numpy.array (if one model) or list (if multiple models)
     :param evaluation_path: numpy.array (if one model) or list (if multiple models)
+    :param n_test_rounds=3: split the arrays into n test rows, only for the purpose of computing confidence bounds only
     :return: none (saved plot)
 
-    TODO:
-    - cutoff point
-    - default on off
-    - write doc string better
-        - uses the sklearn implementation
-    - y_pred to y_score
-    - use common threshold
+    TODO v0.1
+    ---------
 
+    - clean up
+
+    Notes
+    -----
+
+    Computing one ROC per time series is wrong, since the threshold cannot be set time series specific.
+    For example, in a medical setting, the threshold for the classifier cannot be set for each patient individually withour prior knowledge.
+
+    Possible improvements
+    ---------------------
+
+    - input checks for the funciton parameters
+    - instead of looping with j, make more efficient implementation that computes the curves just once and plots 4 times
+    - the variable names tpr and fpr are wrong when computing tnr and fnr (j=3, 4) -> should be corrected for better style
 
     """
-    # new column order: 0:k_th_fold, 1:ts_id, 2:time, 3:y_true, 4:y_pred
-
     # differentiate whether multiple models or one model is used
     # print(type(k_th_fold))
     if type(k_th_fold) == np.ndarray:
         style = 'single'
         # for implementation reasons, we have to wrap the numpy.ndarrays into list if only the arrays are passed
-        k_th_fold_list, ts_id_list, time_list, y_true_list, y_pred_list = [k_th_fold], [ts_id], [time], [y_true], [y_pred]
+        k_th_fold_list, ts_id_list, time_list, y_true_list, y_score_list = [k_th_fold], [ts_id], [time], [y_true], [y_score]
     else:
         style = 'multiple'
         # the parameters of the function are lists of numpy.arrays (except for the evaluation_path)
         # check that the parameters are lists and that the lists are equally long
-        if not (type(k_th_fold)==list and type(ts_id)==list and type(time)==list and type(y_true)==list and type(y_pred)==list) and \
-                (len(k_th_fold) == len(ts_id) == len(time) == len(y_true) == len(y_pred)):
+        if not (type(k_th_fold)==list and type(ts_id)==list and type(time)==list and type(y_true)==list and type(y_score)==list) and \
+                (len(k_th_fold) == len(ts_id) == len(time) == len(y_true) == len(y_score)):
             raise AssertionError
-        k_th_fold_list, ts_id_list, time_list, y_true_list, y_pred_list = k_th_fold, ts_id, time, y_true, y_pred  # plain assignment, since they are already lists
+        k_th_fold_list, ts_id_list, time_list, y_true_list, y_score_list = k_th_fold, ts_id, time, y_true, y_score  # plain assignment, since they are already lists
 
     number_of_tests = len(k_th_fold_list)
     if default:
@@ -107,28 +124,26 @@ def plot_roc(k_th_fold, ts_id, time, y_true, y_pred, model_labels, evaluation_pa
             max_label_index = np.argmax(counts)
             max_label = np.asscalar(all_labels[max_label_index])
             # always predict the max label
-            y_pred_default = np.repeat(max_label, repeats=y_true.shape[0])
+            y_score_default = np.repeat(max_label, repeats=y_true.shape[0])
             # make other dummy arrays as well, from first elements in the list
             k_th_fold, ts_id, time = k_th_fold_list[0], ts_id_list[0], time_list[0]
             k_th_fold_list.append(k_th_fold)
             ts_id_list.append(ts_id)
             time_list.append(time)
             y_true_list.append(y_true)
-            y_pred_list.append(y_pred)
+            y_score_list.append(y_score)
 
         # looping over the models (if single: only one time running this loop)
         for m in range(number_of_tests):
             # will overwrite parameters of functions, but they are no longer needed (since stored in corresponding ..._list variables)
-            k_th_fold, ts_id, time, y_true, y_pred = k_th_fold_list[m], ts_id_list[m], time_list[m], y_true_list[m], y_pred_list[m]
+            k_th_fold, ts_id, time, y_true, y_score = k_th_fold_list[m], ts_id_list[m], time_list[m], y_true_list[m], y_score_list[m]
 
             # find all unique piggs
             # unique_ts_ids = np.unique(ts_id)
 
             # shuffle all arrays consistently
-            k_th_fold, ts_id, time, y_true, y_pred = shuffle(k_th_fold, ts_id, time, y_true, y_pred)
+            k_th_fold, ts_id, time, y_true, y_score = shuffle(k_th_fold, ts_id, time, y_true, y_score)
 
-            # split the arrays into n test rows, only for the purpose of computing confidence bounds
-            n_test_rounds = 3
             # split the indices into n_test_folds parts
             n_rows_total = int(k_th_fold.shape[0])  # int conversion so that indices are integers
             n_rows_part = int(n_rows_total / n_test_rounds)  # int conversion so that indices are integers
@@ -153,7 +168,7 @@ def plot_roc(k_th_fold, ts_id, time, y_true, y_pred, model_labels, evaluation_pa
             base_fpr = np.concatenate((base_fpr_1, base_fpr_2))
             # base_fpr = np.linspace(0, 1, 101)
 
-            # looping over the pigs
+            # looping over artificial test rounds
             for f in range(n_test_rounds):
                 # extract start and end index for all arrays
                 start_index, end_index = row_indices_parts[f]
@@ -162,19 +177,19 @@ def plot_roc(k_th_fold, ts_id, time, y_true, y_pred, model_labels, evaluation_pa
                 ts_id_round = ts_id[start_index:end_index]
                 time_round = time[start_index:end_index]
                 y_true_round = y_true[start_index:end_index]
-                y_pred_round = y_pred[start_index:end_index]
+                y_score_round = y_score[start_index:end_index]
 
 
-                fpr_cur, tpr_cur, thresholds_cur = sklearn.metrics.roc_curve(y_true_round, y_pred_round)
+                fpr_cur, tpr_cur, thresholds_cur = sklearn.metrics.roc_curve(y_true_round, y_score_round)
                 fpr_cur, tpr_cur, thresholds_cur = np.array(fpr_cur), np.array(tpr_cur), np.array(thresholds_cur)
                 if j==3 or j==4:
                     fpr_cur, tpr_cur = convert_to_fnr_tnr(fpr_cur, tpr_cur)
                     fpr_cur, tpr_cur = np.flip(fpr_cur, axis=0), np.flip(tpr_cur, axis=0)
 
-                auc_cur = roc_auc_score(y_true_round, y_pred_round)
+                auc_cur = roc_auc_score(y_true_round, y_score_round)
                 tpr = np.interp(base_fpr, fpr_cur, tpr_cur)
 
-                if j==1:
+                if j==1 or j==3:
                     tpr[0] = 0.0
                 tprs.append(tpr)
                 aucs.append(auc_cur)
@@ -185,6 +200,8 @@ def plot_roc(k_th_fold, ts_id, time, y_true, y_pred, model_labels, evaluation_pa
                     alpha = 1
                 elif style == 'multiple':
                     alpha = 0.3
+                plt.plot(base_fpr, tpr, color=colors[f],  # before fpr_cur, tpr_cur
+                         linewidth=linewidth_pigs, alpha=alpha) #, label=id   alpha=0.15     # label=ts_id + '_ROC curve (AUC = %0.2f)' % (auc_cur),alpha=0.2
 
             #add the ts_id label right next to the line
             # labelLines(plt.gca().get_lines(), zorder=2.5)  # turn on, if label displayed right next to line
@@ -210,12 +227,6 @@ def plot_roc(k_th_fold, ts_id, time, y_true, y_pred, model_labels, evaluation_pa
                 label = ''
             elif style == 'multiple':
                 label = model_labels[m]
-
-            print("HIERO")
-            print(m)
-            print(number_of_tests)
-            print(base_fpr.shape)
-            print(mean_tprs.shape)
 
             # special case: the last ROC plotted is the default prediction
             if default and m == number_of_tests-1:
@@ -249,12 +260,11 @@ def plot_roc(k_th_fold, ts_id, time, y_true, y_pred, model_labels, evaluation_pa
             plt.xlabel('False Negative Rate (log scale)', fontsize='medium')
             ax.set_xscale('log')  # log-scale
 
-        # cut of ROC on the left side where it becomes a flat line on a log scale
+        # cut off ROC on the left side, currently arbitrarily at 1e-3
         if j == 2 or j == 4:
             # left cut-off point is at 1/N, where N is the number of observations
-            cut_off_point = fpr_cur[int(fpr_cur.shape[0]*0.02)] # 2% quantile arbitrarily chosen, since before that often flat line
-            print("cut-off point = " + str(cut_off_point))
-
+            cut_off_point =  1e-3  # base_fpr[20]  # fpr_cur[int(fpr_cur.shape[0]*0.02)] # 2% quantile arbitrarily chosen, since before that often flat line
+            print("cut-off point (xlim left): " + str(cut_off_point))
             # print("cut-off point = " + str(cut_off_point))  # 1/number of observations
             plt.xlim(cut_off_point, 1.0)
 
